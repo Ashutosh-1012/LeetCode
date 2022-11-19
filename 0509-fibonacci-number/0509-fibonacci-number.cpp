1a08a3cc71ba25f35594361e55f8ceb05ca96a0b
Ashutosh kumar Giri <103849806+Ashutosh-1012@users.noreply.github.com>
2022-11-19 22:56:26 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (37.98%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<=1)return n;
-        
-        return (fib(n-1)+fib(n-2));
-    }
+        int prev=0;
+        int curr=1;
+        if(n==0)return prev;
+        if(n==1)return curr;
+        int ans=0;
+        for(int i=2;i<=n;i++){
+            ans=prev+curr;
+            prev=curr;
+            curr=ans;
+        }
+   return ans; }
 };
