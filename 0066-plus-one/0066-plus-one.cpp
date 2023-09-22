7aba6525fdd5c4cbb0980c8a0cabf2124d60fdd0
Ashutosh kumar Giri <103849806+Ashutosh-1012@users.noreply.github.com>
2023-09-22 12:51:24 +0530

    Time: 4 ms (30.37%), Space: 9.2 MB (7.61%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -3,12 +3,13 @@ public:
     vector<int> plusOne(vector<int>& digits) {
  
         for(int i=digits.size()-1;i>=0;i--){
-            if(digits[i]==9){
-                digits[i]=0;
+            if(digits[i]!=9){
+                digits[i]++;
+                return digits;
             }
             else{
-                digits[i]=digits[i]+1;
-                return digits;
+                digits[i]=0;
+                
             }
         }
         digits.push_back(0);
