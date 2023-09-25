78ed179c580588502c9a04e694809a2f96d59942
Ashutosh kumar Giri <103849806+Ashutosh-1012@users.noreply.github.com>
2023-09-26 01:19:02 +0530

    Time: 34 ms (53.78%), Space: 32.5 MB (38.99%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,22 +1,47 @@
 class Solution {
 public:
+     int lowerbond(vector<int>arr, int x) {
+        int start=0;
+        int end=arr.size()-1;
+        int ans=end;
+        while(start<=end){
+          int mid=(start+end)/2;
+          if(arr[mid]>=x){
+            ans=mid;
+            end=mid-1;
+          }
+          else if(arr[mid]<x){
+            start=mid+1;
+          }
+          else{
+            end=mid-1;
+          }
+        }
+        return ans;
+    }
 
- vector<int> Elements(vector<int>& arr, int k, int x) {
-       int start=0;
-       int end=arr.size()-1;
-       while(end-start>=k){
-         if(x-arr[start]>arr[end]-x){
-           start++;
-         }else{
-         end--;}
-       }
-       vector<int>ans;
-       for(int i=start;i<=end;i++){
-         ans.push_back(arr[i]);
-       }
-       return ans;
+    vector<int> bs(vector<int>& arr, int k, int x) {
+        int high=lowerbond(arr,x);
+        int low=high-1;
+        while(k--){
+          if(high>=arr.size()){
+            low--;
+          }
+          else if(low<0){
+            high++;
+          }
+          else if(x-arr[low]>arr[high]-x){
+            high++;
+          }
+          else{
+            low--;
+          }
+        }
+
+        return vector<int>(arr.begin()+low+1,arr.begin()+high);
+
     }
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-       return Elements(arr,k,x);
+        return bs(arr,k,x);
     }
 };
