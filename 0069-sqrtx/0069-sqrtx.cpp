226f5245cee405a18a71b20a80a8f97f22dfc273
Ashutosh kumar Giri <103849806+Ashutosh-1012@users.noreply.github.com>
2023-10-11 05:44:20 +0530

    Time: 3 ms (38.1%), Space: 6.3 MB (30.33%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
-    int mySqrt(int x) {
-        return sqrt(x);
+ int check(int x){
+    if(x==0)return 0;
+    if(x==1)return 1;
+    int start=1;
+    int end=x;
+    // cout<<x;
+    int ans;
+     int mid;
+    while(start<=end){
+        mid=start+(end-start)/2;
+        if(mid==x/mid){
+           return mid;
+        }else if(mid<x/mid){
+            ans=mid;
+            start=mid+1;
+        }else{
+            end=mid-1;
+        }
     }
+    return ans;
+}
+    int mySqrt(int x) {
+        int ans=check(x);
+        cout<<ans;
+        return ans;
+     }
 };
