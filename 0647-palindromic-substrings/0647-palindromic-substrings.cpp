5ed47c16909cb719e070b6df1b71b235dbdaeb21
Ashutosh kumar Giri <103849806+Ashutosh-1012@users.noreply.github.com>
2023-10-12 04:51:11 +0530

    Time: 9 ms (51.54%), Space: 13.7 MB (28.45%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-//check number of the substring is palindrome
    int check(string s,int i,int j){
        int cnt=0;
        while(i>=0 and j<s.length() and s[i]==s[j]){
@@ -8,22 +7,19 @@ public:
            i--;
            j++;
        }
-//return number of substring which is palindrome 
-//which centre is centre i;
        return cnt;
    }
     int countSubstrings(string s) {
+      
       int count=0;
       for(int i=0;i<s.length();i++){
-//it check substring is palindrome which centre is i  and 
-//length is even
+
           int even=check(s,i,i);
-//it check the substring size is odd and centre is i and i+1;
           int odd=check(s,i,i+1);
-//sum of even and odd substring
           count+=even+odd;
       }
-//return total number of palindrome substring
       return count;
+
+        
     }
 };
