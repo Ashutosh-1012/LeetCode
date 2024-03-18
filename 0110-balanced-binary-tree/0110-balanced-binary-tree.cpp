aa1f748666cce5773c4f05adb4643fef384949c2
Ashutosh kumar Giri <103849806+Ashutosh-1012@users.noreply.github.com>
2024-03-18 14:06:53 +0530

    Time: 78 ms (6.64%), Space: 21.8 MB (7.03%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -6,30 +6,34 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
- 
- class Solution {
+class Solution {
 public:
-    bool flag = true;
-
-    int balanced(TreeNode* root) {
-        // if flag is false, do not traverse tree further. means
-        // tree is not balanced.
-        if (root == NULL || !flag) 
+    int height(TreeNode* root) {
+        if (!root) {
             return 0;
-        if (root->left == NULL && root->right ==NULL)
-            return 1;
-        int height_left = balanced(root->left);
-        int height_right = balanced(root->right);
-        if( abs(height_right - height_left)>1 )
-            flag = false;
-        return 1+max(height_left, height_right);
+        }
+        int left = height(root->left);
+        int right = height(root->right);
+        int ans = max(left, right) + 1;
+        return ans;
     }
-
     bool isBalanced(TreeNode* root) {
-        balanced(root);
-        return flag;
+        if (!root) {
+            return true;
+        }
+        int a = height(root->left);
+        int b = height(root->right);
+        bool temp = (abs(a - b) <= 1);
+        bool aa = isBalanced(root->left);
+        bool bb = isBalanced(root->right);
+        if (temp and aa and bb) {
+            return true;
+        } else {
+            return false;
+        }
     }
 };
