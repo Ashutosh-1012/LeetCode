e02d41dc6209afabc875701e0d0bb7b19f754428
Ashutosh kumar Giri <103849806+Ashutosh-1012@users.noreply.github.com>
2025-02-17 05:35:06 +0530

    Time: 3 ms (78.62%), Space: 8.8 MB (66.67%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.cpp
@@ -4,7 +4,9 @@ public:
     void solve(string tiles, int& count, int i){
         count++;
         for(int ind=i; ind<tiles.length(); ind++){
-            if(ind != i && tiles[ind] == tiles[i]) continue;
+            if(ind != i  and tiles[ind] == tiles[i]) {
+                continue;
+            }
             swap(tiles[ind], tiles[i]);
             solve(tiles, count, i+1);
         }
@@ -12,9 +14,9 @@ public:
 
 
     int numTilePossibilities(string tiles) {
-        sort(tiles.begin(), tiles.end());
-        int i = 0, count = -1;
-        solve(tiles, count, i);
-        return count;
+         sort(tiles.begin(),tiles.end());
+        int answer=-1;
+        solve(tiles, answer, 0);
+        return answer;
     }
 };
